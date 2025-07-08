004303fa6899b661dc80942b329a12db6e3f98e6
tanaygupta <tanaygupta06102000@gmail.com>
2025-07-08 17:57:49 +0530

    Added environment variables and code to retrieve environment variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 #This file contains the necessary configurations, keys/tokens for running this application.
 OpenAIKey=os.getenv("OPENAI_KEY")#Enter your openai token for chat gpt operations
-token = os.getenv("DISCORD_VERSABOT")
+token = os.getenv("DISCORD_VERSABOT_TOKEN")
 #Enter user id of sid, T and SH below
 userid={"Sid":1,"Sh":2,"T":3}
 serverid = int(os.getenv("DISCORD_SERVER_TANAY")) #Enter your server id here
